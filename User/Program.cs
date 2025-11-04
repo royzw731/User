105d2c932a1524eaa36bd99889b55753d2969cd8
USER <USER@DESKTOP-2OKNQSC>
2025-11-04 15:06:52 +0200

    Add project files.

---
+++ User/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+Dictionary<string, int> myDict = new Dictionary<string, int>();
+myDict.Add("Apple", 5);
+myDict.Add("Banana", 3);
+
+// כך סורקים אותו
+foreach (var pair in myDict)
+{
+    Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+}
+Dictionary<string, int> HighScores =  new Dictionary<string, int>();
+HighScores.Add("NinjaMan", 9500);
+HighScores.Add("CodeQueen", 12000);
+HighScores.Add("PixelPal", 7200);
+HighScores["NinjaMan"] = 9900;
+
+
+if (HighScores.ContainsKey("Shadow"))
+{
+    Console.WriteLine($"Shadow score: : {HighScores["Shadow"]}");
+}
+else Console.WriteLine(" Shadow has no score yet");
+   foreach (var pair in HighScores)
+{
+    Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+}
+if (HighScores.ContainsKey("PixelPall"))
+{
+    HighScores.Remove("PixelPall");
+}
+foreach (var pair in HighScores)
+{
+    Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+}
---
+++ User/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    internal class User
+    {
+        public String UserName
+        {
+            get; set;
+        }
+        public List<String> friends
+        {
+            get; private set;
+        }
+        public User(String UserName, List<String> friends)
+        {
+            this.UserName = UserName;
+            this.friends = friends;
+
+        } public bool AddFriend(String friendsName)  {
+            if (friends != null && !friends.Contains(friendsName)) 
+                { friends.Add(friendsName);
+                Console.WriteLine("Sucsess" );
+                return true;
+            } return false;
+    }
+        public bool RemoveFriend(String friendsName)
+        {
+            if (friends.Contains(friendsName))
+            {
+                friends.Remove(friendsName);
+                Console.WriteLine("sucsess");
+                return true; 
+            }return false;
+        } public void PrintFriends(String friends)
+        {
+            for (int i = 0; i < friends.Count(); i++)
+            {
+                Console.WriteLine(" this user name friend is " + friends[i]);
+                
+            }
+
+        }
+        public Dictionary<string, List<string>> GetFriendsOfFriends(Dictionary<string, User> network)
+        {
+            Dictionary<string, List<string>> Myfriends = new Dictionary<string, List<string>>();
+            foreach (var friend in friends)
+            {
+                if (network.ContainsKey(friend))
+                    Myfriends.Add(friend, network[friend].friends);
+            }
+            return Myfriends;
+        }
+
+    } }
+
